64c2b31bbe4546a8778881d7d498a555b7823e81
Peter Wildberger <pete.wildberger@gmail.com>
2018-09-17 19:11:29 -0500

    car shape is better

--- src/client/classes/Car.class.ts
+++ src/client/classes/Car.class.ts
@@ -1,44 +1,47 @@
 import * as THREE from 'three';
 export class Car {
-	constructor() {}
-	createCar() {
-		// wheels
+  constructor() {}
+  createCar() {
+    // wheels
 
-		let bodyX: number = 0.3;
-		let bodyY: number = 0.3;
-		let bodyZ: number = 0.6;
-		let car = new THREE.Group();
-		let topGeo = new THREE.BoxGeometry(0.2, 0.3, 0.1);
-		let bodyGeo = new THREE.BoxGeometry(bodyX, bodyY, bodyZ);
-		var carMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
-		var top = new THREE.Mesh(topGeo, carMaterial);
-		var body = new THREE.Mesh(bodyGeo, carMaterial);
-		let wheel_pos = this.wheel_places(bodyX, bodyY, bodyZ);
-		let len: number = Object.keys(wheel_pos).length;
-		for (let i = 0; i < len; i += 1) {
-			let wheelGeo = new THREE.TorusGeometry(0.05, 0.025, 16, 100);
-			let wheelMat = new THREE.MeshBasicMaterial({ color: 'black' });
-			let wheel = new THREE.Mesh(wheelGeo, wheelMat);
-			wheel.rotation.y = Math.PI / 2;
-			console.log(wheel_pos);
-			wheel.position.x = wheel_pos[String(i)].x;
-			wheel.position.z = wheel_pos[String(i)].z;
-			wheel.position.y = -0.14;
-			car.add(wheel);
-		}
+    let bodyX: number = 0.3;
+    let bodyY: number = 0.2;
+    let bodyZ: number = 0.6;
+    let car = new THREE.Group();
+    let topGeo = new THREE.CylinderGeometry(1 / Math.sqrt(2), 1.5 / Math.sqrt(2), 0.75, 4, 1); // size of top can be changed
+    let bodyGeo = new THREE.BoxGeometry(bodyX, bodyY, bodyZ);
+    var carMaterial = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+    topGeo.computeFlatVertexNormals();
+    var top = new THREE.Mesh(topGeo, carMaterial);
+    var body = new THREE.Mesh(bodyGeo, carMaterial);
+    let wheel_pos = this.wheel_places(bodyX, bodyY, bodyZ);
+    let len: number = Object.keys(wheel_pos).length;
+    for (let i = 0; i < len; i += 1) {
+      let wheelGeo = new THREE.TorusGeometry(0.05, 0.025, 16, 100);
+      let wheelMat = new THREE.MeshBasicMaterial({ color: 'black' });
+      let wheel = new THREE.Mesh(wheelGeo, wheelMat);
+      wheel.rotation.y = Math.PI / 2;
+      console.log(wheel_pos);
+      wheel.position.x = wheel_pos[String(i)].x;
+      wheel.position.z = wheel_pos[String(i)].z;
+      wheel.position.y = -0.12;
+      car.add(wheel);
+    }
+    top.scale.set(0.2, 0.2, 0.2);
+    top.rotation.y = Math.PI / 4;
+    top.position.y = 0.175;
+    top.position.z = 0.05;
+    car.add(body);
+    car.add(top);
 
-		top.position.y = 0.25;
-		car.add(body);
-		car.add(top);
-
-		return car;
-	}
-	wheel_places(x: number, y: number, z: number): { [key: string]: any } {
-		return {
-			'0': { x: -x / 2, z: z / 4 },
-			'1': { x: x / 2, z: z / 4 },
-			'2': { x: x / 2, z: -z / 4 },
-			'3': { x: -x / 2, z: -z / 4 }
-		};
-	}
+    return car;
+  }
+  wheel_places(x: number, y: number, z: number): { [key: string]: any } {
+    return {
+      '0': { x: -x / 2, z: z / 4 },
+      '1': { x: x / 2, z: z / 4 },
+      '2': { x: x / 2, z: -z / 4 },
+      '3': { x: -x / 2, z: -z / 4 }
+    };
+  }
 }
